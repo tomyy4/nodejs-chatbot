a62bc82bf2759f6899e7f5da1029527278ddfd8f
tomeu <tomeullompartpiza@gmail.com>
2018-11-22 23:20:13 +0100

    Last scenarios
    insults, anything else dialogs added. Dialog matches regex improved

--- index.js
+++ index.js
@@ -46,37 +46,41 @@ Initial dialog
 bot.dialog('initialDialog', [
     function(session) {
         var msg = new builder.Message(session)
-        .text("Hi! How can I help you")
+        .text("How can I help you?")
         .suggestedActions(
         	builder.SuggestedActions.create(
             	session, [
-                	builder.CardAction.imBack(session, "Show me something", "consult"),
-                    builder.CardAction.imBack(session, "I want to send a gift", "gift"),
-                    builder.CardAction.imBack(session, "goodbye", "goodbye")
+                	builder.CardAction.imBack(session, "Show me the products", "Show me the store"),
+                    builder.CardAction.imBack(session, "I want to send a gift", "I'd like to send a gift"),
+                    builder.CardAction.imBack(session, "goodbye", "Not now, thank you")
                 ]
             ));
         session.send(msg);
         session.endDialog();
     }
-]).triggerAction({matches:/^Hello/i});
+]).triggerAction({matches:/^Hello$|hi$/i});
 
 /*
-
+Goodbye dialog
 */
 
 bot.dialog('goodbyeDialog', [
 	function(session) {
 		session.endDialog("See ya!");
 	}
-]).triggerAction({matches:/^goodbye/i});
+]).triggerAction({matches:/^goodbye|bye$/i});
 
 /*
 Consult something dialog
 */
 
+
+//consult phrases
+
 bot.dialog('consultSomething', [
 	function(session) {
 		session.send("Triggering dialog consult something");
+		session.beginDialog("anythingElseDialog");
 	}
 ]).triggerAction({matches:/^Show.*me.*the.*products$/i});
 
@@ -87,13 +91,35 @@ send a gift to someone dialog
 bot.dialog('sendAGiftDialog', [
 	function(session) {
 		session.send("Triggering send a gift dialog");
+		session.beginDialog("anythingElseDialog");
 	}
-]).triggerAction({matches:/^I.*want.*to.*send.*a.*gift$/i});
+]).triggerAction({matches:/^I.*want.*to.*send.*a.*gift$|gift$/i});
 
 
+/*
+Insults dialog
+*/
 
+bot.dialog('insultsDialog', [
+	function(session) {
+		session.send("Hey! Don't mess up with me! I have feelings too :(");
+	}
+]).triggerAction({matches:/^idiot$|stupid$|asshole$/i});
 
+/*
+Anything else dialog
+*/
 
+bot.dialog('anythingElseDialog', [
+	function(session) {
+		builder.Prompts.choice(session,"Is there anything else I can do for you?", "Yes|No",{ listStyle: 3 });
+	},
+	function(session,results) {
+		if (results.response.index === 0) {
+			session.beginDialog('initialDialog');
+		} else {
+			session.beginDialog('goodbyeDialog');
+		}
 
-
-
+	}
+]);
