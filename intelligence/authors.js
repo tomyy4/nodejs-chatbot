9ae5cb26b8021d6869af9be26c13d9726d1ec38f
tomeu <tomeullompartpiza@gmail.com>
2018-11-26 23:35:49 +0100

    Authors/Books triggers added
    Removed unnecessary files

---
+++ intelligence/authors.js
@@ -0,0 +1,4 @@
+
+var authors = /^authors$|write$|check.*authors$|show.*me.*the.*authors$/i;
+
+module.exports = authors;
---
+++ intelligence/books.js
@@ -0,0 +1,4 @@
+
+var books = /^books$|read$|check.*books$|show.*me.*the.*books$/i
+
+module.exports = books;
