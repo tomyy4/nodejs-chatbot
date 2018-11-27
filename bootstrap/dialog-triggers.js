77c48b0f58b5ac2445db4ad596ff9cd8617283a5
tomeu <tomeullompartpiza@gmail.com>
2018-11-27 13:09:19 +0100

    dialog/intelligence bootstrap

---
+++ bootstrap/dialog-triggers.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	insultsRegexp : require('../intelligence/insults.js'),
+ 	greetingsRegexp : require('../intelligence/greetings.js'),
+ 	farewellRegexp : require('../intelligence/farewell.js'),
+	booksRegexp : require('../intelligence/books.js'),
+ 	authorsRegexp : require('../intelligence/authors.js')
+}
---
+++ bootstrap/require-dialogs.js
@@ -0,0 +1,16 @@
+
+/*
+REQUIRE DIALOGS
+*/
+
+module.exports = {
+	InitialDialog: require('../dialogs/Initial'),
+	AuthorsDialog: require('../dialogs/Authors'),
+	BooksDialog: require('../dialogs/Books'),
+	AnythingElseDialog: require('../dialogs/AnythingElse'),
+	GoodbyeDialog: require('../dialogs/Goodbye'),
+	InsultsDialog: require('../dialogs/Insults'),
+	SendAGiftDialog: require('../dialogs/SendAGift')
+};
+
+
